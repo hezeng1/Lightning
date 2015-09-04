3bed45489e418a3ee83181e642c1b8108df3e309
hezeng1 <herman.zeng114@gmail.com>
2015-09-04 11:36:39 -0700

    fart

--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,45 +14,40 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
-int startX = 150;
-int startY = 0;
-int endX = 150;
-int endY = 0;
-PImage img;
+int startX = 250; 
+int startY = 250; 
+int endX = 0;
+int endY = 150;
+PImage img; 
 public void setup()
 {
-  size(300,300);
-  strokeWeight(2);
-  
-  img = loadImage("Donkey.jpg");
-  
+  strokeWeight(5);
+  background(0);
+  size(350,350);
+  img = loadImage("wario.jpg");
 
 }
 public void draw()
 {
-  background(0);
   image(img,0,0);
   stroke((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
- while(endY<300)
+  while(endX<350)
+  {
+    endX = startX + (int)(Math.random()*10);
+    endY = startY + (int)(Math.random()*18)-9;
+    line(startX,startY,endX,endY);
+    line(startX,startY-50,endX,endY);
+    startX = endX;
+    startY = endY; 
+  } 
+if(endX>300)
 {
-  endY = startY +(int)(Math.random()*10);
-  endX = startX +(int)(Math.random()*18)-9;
-  line(startX,startY,endX,endY);
-  line(startX-100,startY,endX-100,endY);
-  line(startX+100,startY,endX+100,endY);
-  startX = endX;
-  startY = endY;
-}
-if(endY>280)
-{
-  startX = 150;
-  startY = 0;
-  endX = 150;
-  endY = 0; 
+  startX = 250;
+  startY = 250;
+  endX = 0;
+  endY = 150;
 }
 }
-
-
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
